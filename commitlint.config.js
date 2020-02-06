908c35399dc67ad4cc75b73186c9809dc8111c78
Jefferson Vieira da Silva <s2it_jvieira@pagseguro.com>
2020-02-06 11:09:59 -0300

    TEST-001:ujira commit lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,6 @@ module.exports = {
   plugins: ['commitlint-plugin-jira-rules'],
   extends: ['jira'],
   rules: {
-    'jira-task-id-empty': [2, 'always', [' ']],
-    'jira-commit-message-separator': [2, 'always', [' ']]
+    'jira-commit-message-separator': [2, 'always', 'u']
   }
 };
